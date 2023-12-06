dcd5f4f2c34a7179906ba7c0d54baf057ab28078
t2pellet <t2pellet@uwaterloo.ca>
2023-12-06 00:53:38 -0500

    feat: port variable

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,10 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  plugins: [vue(), vueJsx()],
+  server: {
+    port: process.env.PORT ? Number(process.env.PORT) : 3000
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))

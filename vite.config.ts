09dcfe53aac4c5fb95c4e8bdcaf01ecf1a048120
t2pellet <t2pellet@uwaterloo.ca>
2023-12-06 01:01:09 -0500

    feat: just use port 8080

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,6 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
-  server: {
-    port: process.env.PORT ? Number(process.env.PORT) : 3000
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))

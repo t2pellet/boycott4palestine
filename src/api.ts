84cf94a74b161456bc767249576516653094a87d
t2pellet <t2pellet@uwaterloo.ca>
2023-12-07 16:38:13 -0500

    feat: api handlers

---
+++ src/api.ts
@@ -0,0 +1,20 @@
+import axios from 'axios'
+import type { BoycottEntry, BoycottName } from '@/types'
+
+const client = axios.create({
+  baseURL: import.meta.env.VITE_API_ENDPOINT
+})
+
+async function searchNames(name: string): Promise<BoycottName[]> {
+  const result = await client.get('names', {
+    params: { name }
+  })
+  return result.data
+}
+
+async function fetchBoycottEntry(id: number): Promise<BoycottEntry> {
+  const result = await client.get(`boycott/${id}`)
+  return result.data
+}
+
+export { fetchNames, searchNames, fetchBoycottEntry }

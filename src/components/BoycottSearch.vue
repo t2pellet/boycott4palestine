cad95ef4485ff4b6c860510908efb45e8a70e357
t2pellet <t2pellet@uwaterloo.ca>
2023-12-07 16:54:18 -0500

    feat: debounce search

--- src/components/BoycottSearch.vue
+++ src/components/BoycottSearch.vue
@@ -3,31 +3,34 @@ import { type Ref, ref } from 'vue'
 import { OnClickOutside } from '@vueuse/components'
 import { searchNames } from '@/api'
 import type { BoycottName } from '@/types'
+import _debounce from 'lodash/debounce'
 
 let input: Ref<string> = ref('')
 let names: Ref<BoycottName[]> = ref([])
+let showing = ref(false)
 let pending = false
 let hasInput = false
-let showing = false
 
-async function search(event: Event) {
-  pending = true
-  const value = (event.currentTarget as HTMLInputElement).value
-  if (value) {
-    names.value = await searchNames(value)
+const search = _debounce(function (searchValue: string) {
+  if (searchValue) {
+    pending = true
+    searchNames(searchValue).then((value) => {
+      names.value = value
+      pending = false
+    })
   } else names.value = []
-  hasInput = value
-  pending = false
-}
+  hasInput = !!searchValue
+}, 100)
 
 function hide() {
-  if (!input.value || names.value.length) {
-    showing = false
+  if (names.value.length) {
+    console.log('hide')
+    showing.value = false
   }
 }
 
 function show() {
-  showing = true
+  showing.value = true
 }
 </script>
 
@@ -38,18 +41,17 @@ function show() {
         type="text"
         placeholder="Type here"
         v-model="input"
-        @keyup="search"
+        @keyup="(e) => search(e.target.value)"
         @focus="show"
         class="input input-bordered w-full"
       />
-      <div class="absolute bg-base-100 w-full flex flex-col gap-2 rounded-lg p-2" v-if="showing">
+      <div v-if="showing" class="absolute bg-base-100 w-full flex flex-col gap-2 rounded-lg p-2">
         <a class="btn btn-ghost" v-for="name in names" :key="name" :href="`/boycott/${name.id}`">
           {{ name.name }}
         </a>
         <div v-if="hasInput && !pending && !names.length" class="mt-4 text-success text-center">
           No boycotted company found
         </div>
-        <p v-if="pending">Pending</p>
       </div>
     </div>
   </OnClickOutside>

4b7543aa026b8f777feb5597f668c3a76b82e6f9
t2pellet <t2pellet@uwaterloo.ca>
2023-12-12 16:58:00 -0500

    feat: update dark theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: ['./index.html', './src/**/*{.vue,js,ts,jsx,tsx}'],
   plugins: [require('daisyui')],
   daisyui: {
-    themes: ['autumn']
+    themes: ['autumn', 'dim'],
+    darkTheme: 'dim'
   }
 }
